15fce69a2f7e76ff815dc5abbbae896bbb9c6b58
shdab <130516923+shadatr@users.noreply.github.com>
2024-05-14 21:32:02 +0300

    minting added

--- index.ts
+++ index.ts
@@ -1,22 +1,28 @@
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import {
   addConfigLines,
+  mintFromCandyMachineV2,
+  mintV2,
   mplCandyMachine,
+  safeFetchCandyGuard,
+  updateCandyGuard,
 } from "@metaplex-foundation/mpl-candy-machine";
 import {
   createGenericFileFromBrowserFile,
+  dateTime,
   keypairIdentity,
   publicKey,
+  sol,
   some,
 } from "@metaplex-foundation/umi";
 import {} from "@metaplex-foundation/mpl-candy-machine";
 import {
   createNft,
+  fetchMetadata,
   mplTokenMetadata,
   TokenStandard,
 } from "@metaplex-foundation/mpl-token-metadata";
 import { create } from "@metaplex-foundation/mpl-candy-machine";
-import { generateSigner, percentAmount } from "@metaplex-foundation/umi";
 import base58 from "bs58";
 import {
   fetchCandyMachine,
@@ -24,7 +30,13 @@ import {
 } from "@metaplex-foundation/mpl-candy-machine";
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
 import fs from "fs";
-import {CANDY_MACHINE_PUBLIC_KEY, PRIVATE_KEY} from "./env"
+import { CANDY_MACHINE_PUBLIC_KEY, COLLECTION_NFT, PRIVATE_KEY } from "./env";
+import { setComputeUnitLimit } from "@metaplex-foundation/mpl-toolbox";
+import {
+  transactionBuilder,
+  generateSigner,
+  percentAmount,
+} from "@metaplex-foundation/umi";
 
 const QUICKNODE_RPC =
   "https://still-attentive-mountain.solana-devnet.quiknode.pro/39285c1ec1227a1405f74b521cdc0ba8ade76fdc/";
@@ -34,11 +46,7 @@ const umi = createUmi(QUICKNODE_RPC)
 umi.use(irysUploader());
 
 const keypair = umi.eddsa.createKeypairFromSecretKey(
-  Uint8Array.from(
-    base58.decode(
-        PRIVATE_KEY
-    )
-  )
+  Uint8Array.from(base58.decode(PRIVATE_KEY))
 );
 umi.use(keypairIdentity(keypair));
 
@@ -75,7 +83,7 @@ const CreateNftAndCandyMachine = async () => {
         collectionUpdateAuthority: umi.identity,
         tokenStandard: TokenStandard.NonFungible,
         sellerFeeBasisPoints: percentAmount(9.99, 2),
-        itemsAvailable: 5000,
+        itemsAvailable: 10,
         creators: [
           {
             address: umi.identity.publicKey,
@@ -90,10 +98,22 @@ const CreateNftAndCandyMachine = async () => {
           uriLength: 200,
           isSequential: false,
         }),
+        guards: {
+          botTax: some({
+            lamports: sol(0.01),
+            lastInstruction: true,
+          }),
+          solPayment: some({
+            lamports: sol(1.5),
+            destination: umi.identity.publicKey,
+          }),
+          startDate: some({ date: dateTime("2024-01-24T15:30:00.000Z") }),
+        },
+        groups: [],
       })
-    ).sendAndConfirm(umi);
+    ).sendAndConfirm(umi, { send: { skipPreflight: true } });
     console.log(
-      "CreateCandyMachine",
+      "CreateCandyMachine: ",
       base58.encode(Uint8Array.from(CreateCandyMachine.signature))
     );
   } catch (err) {
@@ -103,30 +123,20 @@ const CreateNftAndCandyMachine = async () => {
 // CreateNftAndCandyMachine();
 
 const FetchCandyMachine = async () => {
-  const candyMachineAddress = publicKey(
-    CANDY_MACHINE_PUBLIC_KEY
-  );
+  const candyMachineAddress = publicKey(CANDY_MACHINE_PUBLIC_KEY);
   const candyMachine = await fetchCandyMachine(umi, candyMachineAddress);
   const candyGuard = await fetchCandyGuard(umi, candyMachine.mintAuthority);
 
-  //   candyMachine.publicKey; // The public key of the Candy Machine account.
-  //   candyMachine.mintAuthority; // The mint authority of the Candy Machine which, in most cases, is the Candy Guard address.
-  //   candyMachine.data.itemsAvailable; // Total number of NFTs available.
-  //   candyMachine.itemsRedeemed; // Number of NFTs minted.
-  //   candyMachine.items[0].index; // The index of the first loaded item.
-  //   candyMachine.items[0].name; // The name of the first loaded item (with prefix).
-  //   candyMachine.items[0].uri; // The URI of the first loaded item (with prefix).
-  //   candyMachine.items[0].minted; // Whether the first item has been minted.
-
   console.log(candyMachine);
+  console.log(candyGuard);
 };
 
-FetchCandyMachine();
+// FetchCandyMachine();
 
 const uploadNft = async () => {
   // Read the local image file
   const imageBlob = fs.readFileSync(
-    "/Users/shada/Documents/candy-machine-nfts-collection/image3.png"
+    "/Users/shada/Documents/candy-machine-nfts-collection/image10.png"
   );
 
   // Create a File object from the image blob
@@ -140,7 +150,7 @@ const uploadNft = async () => {
 
   // Upload the JSON metadata
   const uri = await umi.uploader.uploadJson({
-    name: "My NFT #3",
+    name: "My NFT #10",
     description: "My description",
     image: imageUri,
   });
@@ -151,18 +161,100 @@ const uploadNft = async () => {
 // uploadNft();
 
 const AddItemsToCandyMachine = async () => {
-  const candyMachineAddress = publicKey(
-    CANDY_MACHINE_PUBLIC_KEY
-  );
+  const candyMachineAddress = publicKey(CANDY_MACHINE_PUBLIC_KEY);
   await addConfigLines(umi, {
     candyMachine: candyMachineAddress,
     index: 0,
     configLines: [
-      { name: "My NFT #1", uri: "https://arweave.net/fKNh_IDlWg3FWgBzz2PFncyEnINof2meG5WR70m1kZI" },
-      { name: "My NFT #2", uri: "https://arweave.net/wCR2h-eaPFSrk9PkGVYk2U-dl0fjPD0zh5gJmXdBBtE" },
-      { name: "My NFT #3", uri: "https://arweave.net/d5SNRlvM_4jTyfaQxGpF85Uku52hmUH9LZazXNFy-uU" },
+      {
+        name: "My NFT #1",
+        uri: "https://arweave.net/fKNh_IDlWg3FWgBzz2PFncyEnINof2meG5WR70m1kZI",
+      },
+      {
+        name: "My NFT #2",
+        uri: "https://arweave.net/wCR2h-eaPFSrk9PkGVYk2U-dl0fjPD0zh5gJmXdBBtE",
+      },
+      {
+        name: "My NFT #3",
+        uri: "https://arweave.net/d5SNRlvM_4jTyfaQxGpF85Uku52hmUH9LZazXNFy-uU",
+      },
+      {
+        name: "My NFT #4",
+        uri: "https://arweave.net/v_P0Tkvg1cFDQmW-WiyEI4hXNsQpubrFbUJ5MCidpYw",
+      },
+      {
+        name: "My NFT #5",
+        uri: "https://arweave.net/1-cwCJEVEXTuhLFF9EvwZtGpmsP9ko8nXuKGL1VDq9M",
+      },
+      {
+        name: "My NFT #6",
+        uri: "https://arweave.net/zmjGcNlvP0hY18wQ4xOIa9oSN0BdMMTV35XJ6syTHSM",
+      },
+      {
+        name: "My NFT #7",
+        uri: "https://arweave.net/A93i1CPaaaLWGojYTDu9uE1iCgMg3CfYaaOA00Ad4Fw",
+      },
+      {
+        name: "My NFT #8",
+        uri: "https://arweave.net/JoIQX59v6t79L8NlGiSr6ckCHIS_OsDSRTTPsMz_O74",
+      },
+      {
+        name: "My NFT #9",
+        uri: "https://arweave.net/cvfhBU5iAtjrUChOGiLWyG-GWSEWWzwqXJOgSckVrKQ",
+      },
+      {
+        name: "My NFT #10",
+        uri: "https://arweave.net/iD0Fv2Gk1X9VVqvpwLzvwNXRckthF2do060jF8fztGw",
+      },
     ],
   }).sendAndConfirm(umi);
 };
 
 // AddItemsToCandyMachine()
+
+const MintNft = async () => {
+  const candyMachineAddress = publicKey(CANDY_MACHINE_PUBLIC_KEY);
+  const candyMachine = await fetchCandyMachine(umi, candyMachineAddress);
+  const nftMint = generateSigner(umi);
+  const metadata = await fetchMetadata(umi, publicKey(COLLECTION_NFT));
+
+  await transactionBuilder()
+    .add(setComputeUnitLimit(umi, { units: 800_000 }))
+    .add(
+      mintV2(umi, {
+        candyMachine: candyMachine.publicKey,
+        nftMint,
+        collectionMint:publicKey(COLLECTION_NFT),
+        collectionUpdateAuthority: metadata.updateAuthority,
+        tokenStandard: candyMachine.tokenStandard,
+      })
+    )
+    .sendAndConfirm(umi);
+};
+
+// MintNft()
+
+const updateCandyMachineAndGuard = async () => {
+  const candyMachineAddress = publicKey(CANDY_MACHINE_PUBLIC_KEY);
+  const candyMachine = await fetchCandyMachine(umi, candyMachineAddress);
+  const candyGuard = await fetchCandyGuard(umi, candyMachine.mintAuthority);
+  const updateCandy = await updateCandyGuard(umi, {
+    candyGuard: candyGuard.publicKey,
+    guards: {
+      botTax: some({
+        lamports: sol(0.01),
+        lastInstruction: true,
+      }),
+      solPayment: some({
+        lamports: sol(1.5),
+        destination: umi.identity.publicKey,
+      }),
+      startDate: some({ date: dateTime("2024-01-24T15:30:00.000Z") }),
+    },
+    groups: [],
+  }).sendAndConfirm(umi, { send: { skipPreflight: true } });
+  console.log(updateCandy);
+  console.log(candyGuard.guards.startDate);
+};
+
+updateCandyMachineAndGuard();
